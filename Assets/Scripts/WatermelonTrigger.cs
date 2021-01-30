bc7fdaea4cb5af489cc329a57872732d97adee8a
fbennert <33483595+fbennert@users.noreply.github.com>
2021-01-30 21:15:35 +0100

    Watermelon Drop hinzugefügt

---
+++ Assets/Scripts/WatermelonTrigger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatermelonTrigger : MonoBehaviour
+{
+    private bool triggered = false;
+
+    public List<GameObject> rbObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!triggered && other.CompareTag("Player"))
+        {
+            triggered = true;
+
+            for (int i = 0; i < rbObjects.Count; i++)
+            {
+                Rigidbody rb = rbObjects[i].AddComponent<Rigidbody>();
+
+                rb.AddForce(new Vector3(Random.Range(0, 3), .5f, 0), ForceMode.Impulse);
+            }
+        }
+    }
+}
